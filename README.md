ce5f556b6295be8c75319a2b72185b67bee52228
cjihrig <cjihrig@gmail.com>
2017-04-04 12:14:37 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # lts
+
 Generate the Node.js LTS schedule
+
+```
+node bin/lts.js -s 2017-04-01 -e 2019-04-01 -h output.html -g output.svg -p output.png
+```
---
+++ bin/lts.js
@@ -0,0 +1,78 @@
+#!/usr/bin/env node
+'use strict';
+const Path = require('path');
+const Bossy = require('bossy');
+const Lib = require('../lib');
+const now = new Date();
+const oneYearFromNow = new Date();
+
+oneYearFromNow.setFullYear(now.getFullYear() + 1);
+
+const cliArgs = {
+  'd': {
+    description: 'Input LTS JSON file',
+    alias: 'data',
+    type: 'string',
+    require: false,
+    multiple: false,
+    default: Path.resolve(__dirname, '..', 'lts.json')
+  },
+  's': {
+    description: 'Query start date',
+    alias: 'start',
+    type: 'string',
+    require: false,
+    multiple: false,
+    default: now
+  },
+  'e': {
+    description: 'Query end date',
+    alias: 'end',
+    type: 'string',
+    require: false,
+    multiple: false,
+    default: oneYearFromNow
+  },
+  'h': {
+    description: 'HTML output file',
+    alias: 'html',
+    type: 'string',
+    require: false,
+    multiple: false,
+    default: null
+  },
+  'g': {
+    description: 'SVG output file',
+    alias: 'svg',
+    type: 'string',
+    require: false,
+    multiple: false,
+    default: null
+  },
+  'p': {
+    description: 'PNG output file',
+    alias: 'png',
+    type: 'string',
+    require: false,
+    multiple: false,
+    default: null
+  }
+};
+
+const args = Bossy.parse(cliArgs, { argv: process.argv });
+
+if (args instanceof Error) {
+  Bossy.usage(cliArgs, args.message);
+  process.exit(1);
+}
+
+const options = {
+  data: require(args.data),
+  queryStart: new Date(args.start),
+  queryEnd: new Date(args.end),
+  html: args.html ? Path.resolve(args.html) : null,
+  svg: args.svg ? Path.resolve(args.svg) : null,
+  png: args.png ? Path.resolve(args.png) : null
+};
+
+Lib.create(options);
